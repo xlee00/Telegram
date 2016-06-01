cbb3da21c6d739487bbc0914a185717a800513ce
Eric <605733730@qq.com>
2016-06-01 17:37:23 +0800

    Add persional message to make telegram build successfully and run successfully

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -12,14 +12,14 @@ public class BuildVars {
     public static boolean DEBUG_VERSION = false;
     public static int BUILD_VERSION = 803;
     public static String BUILD_VERSION_STRING = "3.9";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 39543; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "95d357a10935f9907273c9182780f046"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "cfc39649a2f545e284fc45e9314338b5";
+    public static String HOCKEY_APP_HASH_DEBUG = "d5d18a09dc62c74277058a14396df829";
     public static String GCM_SENDER_ID = "760348033672";
-    public static String SEND_LOGS_EMAIL = "email@gmail.com";
-    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
-    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
-    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String SEND_LOGS_EMAIL = "zhihuali1991@gmail.com";
+    public static String BING_SEARCH_KEY = "https://datamarket.azure.com/account/keys"; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = "OVHXPF3KY0HYOZA0OMZLDXSWOINYIKH1SQKQ50ODMK5W2JY3"; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = "A5VLQF5SY0XGKIS23UVOZHR5THYUJQTE0F0DYORC0RKY02UR"; //obtain your own API_ID at https://developer.foursquare.com/
     public static String FOURSQUARE_API_VERSION = "20150326";
 }
